4b0d170163803f90c1315170f0bdcafc23397692
Kaung Sint <sintkuan@gmail.com>
2019-12-03 02:00:38 -0500

    labs

---
+++ Lab/Lab23Q1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Lab23Q1
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a file of scores: ");
+        File file = new File(input.nextLine());
+        if (file.exists())
+        {
+            int num = 0;
+            double total = 0;
+            try
+            {
+                Scanner f = new Scanner(file);  
+                while (f.hasNext())
+                {
+                    num++;
+                    total += f.nextInt();
+                }
+            }
+            catch (IOException ioe)
+            {}
+
+		    System.out.println("Total scores: " + total);
+		    System.out.println("Average scores: " + (total / num));
+        }
+        else
+            System.out.print("File " + file + " does not exist");
+    }
+}
---
+++ Lab/Lab23Q2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Lab23Q2
+{
+    public static void main(String[] args) throws Exception
+    {
+        File cities = new File("cities.txt");
+        FileWriter fw = new FileWriter(cities, false);
+        PrintWriter pw = new PrintWriter(fw);
+        pw.print("Dallas Houston Chicago Denver Tampa Miami Charlotte Austin Detroit Seattle");
+            
+        pw.flush();
+        pw.close();
+
+        ArrayList<String> c = new ArrayList<>();
+
+        Scanner fs = new Scanner(cities);
+        while (fs.hasNext())
+        {
+            c.add(fs.next());
+        }
+        File output = new File("output.txt");
+        FileWriter fw2 = new FileWriter(output, false);
+        PrintWriter pw2 = new PrintWriter(fw2);
+        for (int i=c.size()-1;i>=0;i--)
+        {
+            pw2.println(c.get(i));
+        }
+            
+        pw2.flush();
+        pw2.close();
+
+    }
+}
